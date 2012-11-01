5389ee241508aee4db9beb3526ffeb59ff99a310
Emre Turan <turan.emre@gmail.com>
2012-11-01 12:56:11 +0200

    Changed UserBsonId

--- restful/modules/Domain/Models/CourseModel.cs
+++ restful/modules/Domain/Models/CourseModel.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace Domain
 {
     public class CourseModel
     {
         [BsonId]
-        public string _id { get; set; }
+        public ObjectId _id { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public string Place { get; set; }
