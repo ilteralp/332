8d03dea7c91a9f37c50ad6686201e47aa616961c
Emre Turan <turan.emre@gmail.com>
2012-12-02 01:04:49 +0200

    new restful project
    it is not working!!!!!

---
+++ restful/restfulapi/Api/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace restfulapi.Api
+{
+    public class UsersController : ApiController
+    {
+        public string Get()
+        {
+            return "emre";
+        }
+    }
+}
